2da08fd08686769337aeaf310bc5da64d83e08db
alexsanya <alexsanyakoval@gmail.com>
2025-01-02 18:30:10 -0500

    refactoring

---
+++ abis/erc20.ts
@@ -0,0 +1,7 @@
+export const ERC20_ABI = [
+    "function balanceOf(address) public view returns (uint256)",
+    "function decimals() public returns (uint256)",
+    "function transfer(address,uint256) public",
+    "function approve(address,uint256) public",
+    "function mint(uint256) public"
+];
--- main.ts
+++ main.ts
@@ -1,75 +1,56 @@
 import { computePoolAddress, FeeAmount, MintOptions, nearestUsableTick, NonfungiblePositionManager } from '@uniswap/v3-sdk'
 import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json';
-import { Token, BigintIsh, Percent, NONFUNGIBLE_POSITION_MANAGER_ADDRESSES } from '@uniswap/sdk-core'
+import { Token, BigintIsh, Percent } from '@uniswap/sdk-core'
 import { Pool, Position } from '@uniswap/v3-sdk'
-import { Contract, ethers, JsonRpcProvider, Wallet } from 'ethers';
+import { Contract, ethers, JsonRpcProvider, Provider, TransactionRequest, Wallet } from 'ethers';
+import { CHAIN_ID, NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS, POOL_FACTORY_CONTRACT_ADDRESS, POOL_FEE, TOKEN_A_ADDRESS, TOKEN_A_DECIMALS, TOKEN_A_MINTER_ADDRESS, TOKEN_B_ADDRESS, TOKEN_B_DECIMALS, TOKEN_B_MINTER_ADDRESS } from './chains/sepolia';
+import { ERC20_ABI } from './abis/erc20';
 
-const SEPOLIA_ID = 11155111;
-const FORK_RPC = "http://127.0.0.1:8545";
-const POOL_FACTORY_CONTRACT_ADDRESS: string = "0x1f2FCf1d036b375b384012e61D3AA33F8C256bbE";
-const NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS: string = "0x544bA588efD839d2692Fc31EA991cD39993c135F";
-const USDC_ADDRESS = "0x1a6922a04b14b1560875d77a8c02ab3c0e354020";
-const DAI_ADDRESS = "0x59e3a6011631de8e5302e5138d7eb3006e607b75";
 
-const token0: Token = new Token(SEPOLIA_ID, USDC_ADDRESS, 6);
-const token1: Token = new Token(SEPOLIA_ID, DAI_ADDRESS, 18);
-const poolFee: FeeAmount = 500;
+const tokenA: Token = new Token(CHAIN_ID, TOKEN_A_ADDRESS, TOKEN_A_DECIMALS);
+const tokenB: Token = new Token(CHAIN_ID, TOKEN_B_ADDRESS, TOKEN_B_DECIMALS);
+const poolFee: FeeAmount = POOL_FEE;
 
-const USDC_VALUE = 100500;
-const DAI_VALUE = 100500000000;
-const ACTOR_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-
-const USDC_ABI = [
-    "function balanceOf(address) public view returns (uint256)",
-    "function transfer(address,uint256) public",
-    "function approve(address,uint256) public",
-    "function mint(uint256) public"
-];
-
-const provider = new JsonRpcProvider(FORK_RPC);
-const actor = new Wallet(ACTOR_KEY, provider);
+const USER_PRIVATE_KEY: string = process.env['USER_PRIVATE_KEY'] as string;
 
 const MAX_FEE_PER_GAS = 250000000000;
 const MAX_PRIORITY_FEE_PER_GAS = 250000000000;
 
-(async function main() {
+function getPoolContract(provider: Provider): Contract {
     const currentPoolAddress = computePoolAddress({
         factoryAddress: POOL_FACTORY_CONTRACT_ADDRESS,
-        tokenA: token0,
-        tokenB: token1,
+        tokenA,
+        tokenB,
         fee: poolFee,
     });
-    console.log({ currentPoolAddress });
-    const poolContract = new ethers.Contract(
+    return new ethers.Contract(
         currentPoolAddress,
         IUniswapV3PoolABI.abi,
         provider
-      )
-      
+    );
+}
+
+async function getConfiguredPool(poolContract: Contract): Promise<Pool> {
     const [liquidity, slot0] =
         await Promise.all([
           poolContract.liquidity(),
           poolContract.slot0(),
-        ])
+        ]);
 
-    console.log({ liquidity, slot0 });
+    console.debug({ liquidity, slot0 });
 
-    console.log('Tick: ', slot0.tick);
-    console.log('SqrtRatio: ', slot0.sqrtPriceX96.toString());
-
-    const configuredPool = new Pool(
-        token0,
-        token1,
+    return new Pool(
+        tokenA,
+        tokenB,
         poolFee,
         slot0.sqrtPriceX96.toString(),
         liquidity.toString(),
         Number(slot0.tick)
     );
+}
 
-    const amount0: BigintIsh = "9990000000000";
-    const amount1: BigintIsh = "10500090000000000000000000";
-
-    const position = Position.fromAmounts({
+function getPosition(configuredPool: Pool, amountA: BigintIsh, amountB: BigintIsh): Position {
+    return Position.fromAmounts({
         pool: configuredPool,
         tickLower:
           nearestUsableTick(configuredPool.tickCurrent, configuredPool.tickSpacing) -
@@ -77,59 +58,104 @@ const MAX_PRIORITY_FEE_PER_GAS = 250000000000;
         tickUpper:
           nearestUsableTick(configuredPool.tickCurrent, configuredPool.tickSpacing) +
           configuredPool.tickSpacing * 2,
-        amount0: amount0,
-        amount1: amount1,
+        amount0: amountA,
+        amount1: amountB,
         useFullPrecision: true,
     })
+}
 
-    const mintOptions: MintOptions = {
-        recipient: actor.address,
+function getMintOptions(recipient: string): MintOptions {
+    return {
+        recipient,
         deadline: Math.floor(Date.now() / 1000) + 60 * 20,
         slippageTolerance: new Percent(50, 10_000),
     }
+}
+
+async function supplyAndApproveTokens(provider: JsonRpcProvider, user: Wallet, amountA: BigintIsh, amountB: BigintIsh): Promise<void> {
+
+    // Impersonate the token holder
+    await provider.send('anvil_impersonateAccount', [TOKEN_A_MINTER_ADDRESS]);
+    if (TOKEN_A_ADDRESS !== TOKEN_B_ADDRESS) {
+        await provider.send('anvil_impersonateAccount', [TOKEN_B_MINTER_ADDRESS]);
+    }
+
+    // Get the token holder signer
+    const minterA = await provider.getSigner(TOKEN_A_MINTER_ADDRESS);
+    const minterB = await provider.getSigner(TOKEN_B_MINTER_ADDRESS);
+
+    // Connect signed with the contract
+    const erc20Acontract = new Contract(TOKEN_A_ADDRESS, ERC20_ABI, minterA);
+    const erc20Bcontract = new Contract(TOKEN_B_ADDRESS, ERC20_ABI, minterB);
+    const [balanceAbefore, balanceBbefore] = await Promise.all([
+        erc20Acontract.balanceOf(user.address),
+        erc20Bcontract.balanceOf(user.address)
+    ]);
+    // mint and transfer
+    const [mintAtx, mintBtx] = await Promise.all([
+        erc20Acontract.mint(amountA),
+        erc20Bcontract.mint(amountB)
+    ]);
+    await Promise.all([ mintAtx.wait(), mintBtx.wait() ]);
+    const [transferA, transferB] = await Promise.all([
+        erc20Acontract.transfer(user.address, amountA),
+        erc20Bcontract.transfer(user.address, amountB)
+    ]);
+    await Promise.all([ transferA.wait(), transferB.wait() ]);
+
+    const [balanceAafter, balanceBafter] = await Promise.all([
+        erc20Acontract.balanceOf(user.address),
+        erc20Bcontract.balanceOf(user.address)
+    ]);
+    console.debug({ balanceAbefore, balanceAafter });
+    console.debug({ balanceBbefore, balanceBafter });
+
+    //approvals
+    //const [approveA, approveB] = await Promise.all([
+    await (erc20Acontract.connect(user) as Contract).approve(NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS, balanceAafter);
+    await (erc20Bcontract.connect(user) as Contract).approve(NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS, balanceBafter);
+    //]);
+
+    //await Promise.all([ approveA.wait(), approveB.wait() ]);
+}
+
+function buildMintTransaction(from: string, position: Position): TransactionRequest {
     const { calldata, value } = NonfungiblePositionManager.addCallParameters(
         position,
-        mintOptions
+        getMintOptions(from)
     )
-    console.log({ calldata, value });
-
-    const tokensOwner = "0x55Dc5ce95849273F522Fe914D3fa035d9E380d4A";
-      // Impersonate the token holder
-  await provider.send('anvil_impersonateAccount', [tokensOwner]);
-
-  // Get the token holder signer
-  const signer = await provider.getSigner(tokensOwner);
-
-  // Connect signed with the contract
-  const usdcContract = new Contract(USDC_ADDRESS, USDC_ABI, signer);
-  const daiContract = new Contract(DAI_ADDRESS, USDC_ABI, signer);
-  const usdcBalanceBefore = await usdcContract.balanceOf(actor.address);
-  const daiBalanceBefore = await daiContract.balanceOf(actor.address);
-  await usdcContract.mint(amount0);
-  await usdcContract.transfer(actor.address, amount0);
-  await daiContract.mint(amount1);
-  await daiContract.transfer(actor.address, amount1);
-
-  const usdcBalanceAfter = await usdcContract.balanceOf(actor.address);
-  const daiBalanceAfter = await daiContract.balanceOf(actor.address);
-  console.log({ usdcBalanceBefore, usdcBalanceAfter });
-  console.log({ daiBalanceBefore, daiBalanceAfter });
-
-//approvals
-    await usdcContract.connect(actor).approve(NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS, amount0);
-    await daiContract.connect(actor).approve(NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS, amount1);
-  
-
-    const transaction = {
+    console.debug({ calldata, value });
+    return {
         data: calldata,
         to: NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS,
         value: value,
-        from: actor.address,
+        from,
         maxFeePerGas: MAX_FEE_PER_GAS,
         maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
-      }
+    }
+}
 
-    const txRes = await actor.sendTransaction(transaction);
+async function mintPosition(amountA: BigintIsh, amountB: BigintIsh): Promise<void> {
+    const provider = new JsonRpcProvider(process.env['PROVIDER_RPC']);
+    const user = new Wallet(USER_PRIVATE_KEY, provider);
+
+    console.log(`User's wallet address: ${user.address}`);
+    const poolContract = getPoolContract(provider); 
+    console.log(`Pool address: ${poolContract.address}`);
+    const configuredPool = await getConfiguredPool(poolContract);
+
+    await supplyAndApproveTokens(provider, user, amountA, amountB);
+
+    const position = getPosition(configuredPool, amountA, amountB);
+    const transaction = buildMintTransaction(user.address, position);
+
+    const txRes = await user.sendTransaction(transaction);
     console.log({ txRes });
 
+}
+
+(async function main() {
+    const amountA: BigintIsh = "9990000000000";
+    const amountB: BigintIsh = "10500090000000000000000000";
+    await mintPosition(amountA, amountB);
 })()
