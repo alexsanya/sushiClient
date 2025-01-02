8b28cb3735a84291b46dcca3b5d78dee1fd665ca
alexsanya <alexsanyakoval@gmail.com>
2025-01-02 16:43:53 -0500

    supply tokens

--- main.ts
+++ main.ts
@@ -2,16 +2,31 @@ import { computePoolAddress, FeeAmount, MintOptions, nearestUsableTick, Nonfungi
 import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json';
 import { Token, BigintIsh, Percent } from '@uniswap/sdk-core'
 import { Pool, Position } from '@uniswap/v3-sdk'
-import { ethers, JsonRpcProvider } from 'ethers';
+import { Contract, ethers, JsonRpcProvider, Wallet } from 'ethers';
 
 const SEPOLIA_ID = 11155111;
+const FORK_RPC = "http://127.0.0.1:8545";
 const POOL_FACTORY_CONTRACT_ADDRESS: string = "0x1f2FCf1d036b375b384012e61D3AA33F8C256bbE";
 const USDC_ADDRESS = "0x1a6922a04b14b1560875d77a8c02ab3c0e354020";
 const DAI_ADDRESS = "0x59e3a6011631de8e5302e5138d7eb3006e607b75";
 
 const token0: Token = new Token(SEPOLIA_ID, USDC_ADDRESS, 6);
-const token1: Token = new Token(SEPOLIA_ID, DAI_ADDRESS, 6);
+const token1: Token = new Token(SEPOLIA_ID, DAI_ADDRESS, 18);
 const poolFee: FeeAmount = 500;
+
+const USDC_VALUE = 100500;
+const DAI_VALUE = 100500000000;
+const ACTOR_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+const USDC_ABI = [
+    "function balanceOf(address) public view returns (uint256)",
+    "function transfer(address,uint256) public",
+    "function mint(uint256) public"
+];
+
+const provider = new JsonRpcProvider(FORK_RPC);
+
+
 
 (async function main() {
     const currentPoolAddress = computePoolAddress({
@@ -21,7 +36,6 @@ const poolFee: FeeAmount = 500;
         fee: poolFee,
     });
     console.log({ currentPoolAddress });
-    const provider = new JsonRpcProvider(process.env['SEPOLIA_RPC'] as string);
     const poolContract = new ethers.Contract(
         currentPoolAddress,
         IUniswapV3PoolABI.abi,
@@ -45,10 +59,8 @@ const poolFee: FeeAmount = 500;
         poolFee,
         slot0.sqrtPriceX96.toString(),
         liquidity.toString(),
-        slot0.tick
+        Number(slot0.tick)
     );
-
-    console.log('Here>>>>>>>')
 
     const amount0: BigintIsh = "9990000000000";
     const amount1: BigintIsh = "10500090000000000000000000";
@@ -75,8 +87,31 @@ const poolFee: FeeAmount = 500;
         position,
         mintOptions
     )
-
     console.log({ calldata, value });
 
+    const tokensOwner = "0x55Dc5ce95849273F522Fe914D3fa035d9E380d4A";
+      // Impersonate the token holder
+  await provider.send('anvil_impersonateAccount', [tokensOwner]);
+
+  // Get the token holder signer
+  const signer = await provider.getSigner(tokensOwner);
+
+  // Connect signed with the contract
+  const usdcContract = new Contract(USDC_ADDRESS, USDC_ABI, signer);
+  const daiContract = new Contract(DAI_ADDRESS, USDC_ABI, signer);
+  const actor = new Wallet(ACTOR_KEY, provider);
+  const usdcBalanceBefore = await usdcContract.balanceOf(actor.address);
+  const daiBalanceBefore = await daiContract.balanceOf(actor.address);
+  await usdcContract.mint(USDC_VALUE);
+  await usdcContract.transfer(actor.address, USDC_VALUE);
+  await daiContract.mint(DAI_VALUE);
+  await daiContract.transfer(actor.address, DAI_VALUE);
+
+  const usdcBalanceAfter = await usdcContract.balanceOf(actor.address);
+  const daiBalanceAfter = await daiContract.balanceOf(actor.address);
+  console.log({ usdcBalanceBefore, usdcBalanceAfter });
+  console.log({ daiBalanceBefore, daiBalanceAfter });
+
+  
 
 })()
